b810c66849eb30b4e58506adcca6bfcc50aa3ad1
Tushar Parimi <93556280+tusharparimi@users.noreply.github.com>
2022-04-07 17:20:10 -0400

    Add files via upload

---
+++ SQL/Top Competitors/top_competitors.sql
@@ -0,0 +1,8 @@
+SELECT 
+(CASE
+WHEN g.Grade>=8 THEN s.Name
+ElSE NULL
+END),g.Grade,s.Marks
+FROM Students s CROSS JOIN Grades g
+WHERE s.Marks BETWEEN g.Min_Mark AND g.Max_Mark
+ORDER BY g.Grade DESC,s.Name,s.Marks;
